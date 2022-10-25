b7293c3b9b5bc76aa8d407d54ddbb85a521ca395
Andre Marcos <andrmgc@gmail.com>
2022-10-25 17:44:53 -0300

    req 07

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import ValidateLogin from '../middlewares/login.validate';
+import ValidateUser from '../middlewares/user.validate';
 
 const userRouter = Router();
 const userController = new UserController();
 const validateLoginInfo = new ValidateLogin();
+const validateUserInfo = new ValidateUser();
 
-userRouter.post('/users', userController.createNewUser);
+userRouter.post(
+  '/users', 
+  validateUserInfo.validateUserClasse,
+  validateUserInfo.validateUserLevel,
+  validateUserInfo.validateUserName,
+  validateUserInfo.validateUserPassword, 
+  userController.createNewUser,
+);
 userRouter.post('/login', validateLoginInfo.validateUserLogin, userController.login);
 
 export default userRouter;
