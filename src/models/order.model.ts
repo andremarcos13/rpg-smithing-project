907403957c819dbf8a79816c483dcb4996d1e07a
Andre Marcos <andrmgc@gmail.com>
2022-10-25 14:10:35 -0300

    order model getall

---
+++ src/models/order.model.ts
@@ -0,0 +1,24 @@
+import { Pool } from 'mysql2/promise';
+import { IOrder } from '../interfaces/interface';
+
+export default class OrderModel {
+  connection: Pool;
+    
+  constructor(connection:Pool) {
+    this.connection = connection;
+  }
+
+  public async getAllOrders():Promise<IOrder[]> {
+    const [orders] = await this.connection
+      .execute(`
+      SELECT o.id, o.userId, JSON_ARRAYAGG(p.id) AS productsIds
+         FROM Trybesmith.Orders as o
+        INNER JOIN 
+      Trybesmith.Products AS p
+    ON o.id = p.orderId
+    GROUP BY o.id
+    ORDER BY o.userId
+  `);
+    return orders as IOrder[];
+  }
+}
